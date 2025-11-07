08a41b7a714adde199b7c2e2467c8b71cfb6df51
Mehrdad Tajdini <mehrta.cs@gmail.com>
2025-11-07 16:49:26 +0330

    @bootkit/ng0@0.0.0-alpha.33

--- projects/ng0/components/dropdown/dropdown.component.ts
+++ projects/ng0/components/dropdown/dropdown.component.ts
@@ -1,33 +1,39 @@
-import { Component, ElementRef, Renderer2, ChangeDetectionStrategy, inject, input, model, HostListener, ContentChild, booleanAttribute, ViewChild, computed } from '@angular/core';
+import {
+    Component, ElementRef, Renderer2, ChangeDetectionStrategy, inject, input, model, HostListener, ContentChild,
+    booleanAttribute, ViewChild, EventEmitter,
+    Output
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CssClass, Placement } from '@bootkit/ng0/common';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { DropdownAutoCloseBehavior, DropdownSize } from './types';
 import { DropdownMenuComponent } from './dropdown-menu.component';
 
+/**
+ * Dropdown component
+ */
 @Component({
     selector: 'ng0-dropdown',
     exportAs: 'ng0Dropdown',
     templateUrl: './dropdown.component.html',
     standalone: true,
     changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [
-        CommonModule,
-        OverlayModule,
-    ],
+    imports: [CommonModule, OverlayModule],
     host: {
-        '[class]': '_directionCssClass()',
+        '[class.dropup]': 'placement() === "top"',
+        '[class.dropstart]': 'placement() === "start"',
+        '[class.dropend]': 'placement() === "end"',
+        '[class.btn-group]': 'defaultStyle()',
     }
 })
 export class DropdownComponent {
-    // @ContentChildren(DropdownItemComponent) private _items!: QueryList<DropdownItemComponent>;
     @ContentChild(DropdownMenuComponent) private _dropdownMenu!: DropdownMenuComponent;
     @ViewChild('mainButton') private _mainButton!: ElementRef<HTMLButtonElement>;
     @ViewChild('splitButton') private _splitButton?: ElementRef<HTMLButtonElement>;
-    // protected _scrollStrategy!: ScrollStrategy;
     protected _el = inject(ElementRef);
     private _renderer = inject(Renderer2);
-
+    // @ContentChildren(DropdownItemComponent) private _items!: QueryList<DropdownItemComponent>;
+    // protected _scrollStrategy!: ScrollStrategy;
 
     /**
      * The placement of the dropdown menu in relation to the dropdown toggle.
@@ -35,16 +41,23 @@ export class DropdownComponent {
     public placement = input<Placement>('bottom');
 
     /**
+     * Indicates whether the dropdown should use the default Bootstrap styles.
+     * This will apply the 'btn-group' class to the dropdown.
+     * @default true
+     */
+    public defaultStyle = input(true);
+
+    /**
      * The CSS classes to apply to the dropdown toggle button.
      * @default 'btn btn-primary'
      */
-    public cssClass = input<CssClass>('btn btn-primary');
+    public toggleCss = input<CssClass>('btn btn-primary');
 
     /**
      * The CSS classes to apply to the dropdown split button.
      * @default 'btn btn-primary'
      */
-    public splitCssClass = input<CssClass>('btn btn-primary');
+    public splitCss = input<CssClass>('btn btn-primary');
 
     /** 
      * Indicates whether the dropdown is open or closed.
@@ -71,22 +84,18 @@ export class DropdownComponent {
      */
     public readonly size = input<DropdownSize>('default');
 
-    protected _directionCssClass = computed(() => {
-        switch (this.placement()) {
-            case 'top':
-                return 'dropup';
-            case 'start':
-                return 'dropstart';
-            case 'end':
-                return 'dropend';
-            case 'bottom':
-            default:
-                return undefined;
-        }
-    })
+    /**
+     * Indicates whether the dropdown is disabled.
+     */
+    public readonly disabled = input(false, { transform: booleanAttribute });
+
+    /**
+    * Emits when the dropdown toggle button is clicked.
+    */
+    @Output() public toggleClick = new EventEmitter<PointerEvent>();
 
     constructor() {
-        this._renderer.addClass(this._el.nativeElement, 'btn-group');
+        // this._renderer.addClass(this._el.nativeElement, 'btn-group');
         // this._scrollStrategy = this._overlay.scrollStrategies.block();
     }
 
