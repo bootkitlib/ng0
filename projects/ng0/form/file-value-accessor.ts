39a43366a7539725c15689441767e36283141464
Mehrdad Tajdini <mehrta.cs@gmail.com>
2026-02-16 14:27:04 +0330

    0.0.0-alpha.50

--- projects/ng0/form/file-value-accessor.ts
+++ projects/ng0/form/file-value-accessor.ts
@@ -1,19 +1,22 @@
 import { Directive, ElementRef, forwardRef, inject, Provider, Renderer2, } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
-export const FILE_VALUE_ACCESSOR: Provider = {
-    provide: NG_VALUE_ACCESSOR,
-    useExisting: forwardRef(() => FileValueAccessor),
-    multi: true,
-};
-
+/**
+ * A ControlValueAccessor for file input elements. It supports both single and multiple file selection.
+ * It assigns the selected file(s) to the form control's value and handles disabled state.
+ * @see https://angular.dev/api/forms/ControlValueAccessor
+ */
 @Directive({
     selector: 'input[type=file][ngModel][ng0File]',
     host: {
         '(blur)': '_touchedFn()',
         '(change)': '_onChange($event)',
     },
-    providers: [FILE_VALUE_ACCESSOR],
+    providers: [{
+        provide: NG_VALUE_ACCESSOR,
+        useExisting: forwardRef(() => FileValueAccessor),
+        multi: true,
+    }],
 })
 export class FileValueAccessor implements ControlValueAccessor {
     private readonly _renderer = inject(Renderer2);
@@ -36,13 +39,12 @@ export class FileValueAccessor implements ControlValueAccessor {
         this._renderer.setProperty(this._elementRef.nativeElement, 'disabled', isDisabled);
     }
 
-    _onChange(e: Event) {
+    protected _onChange(e: Event) {
         const input = e.target as HTMLInputElement;
-        const isMultiple = input.multiple;
 
         let value;
         if (input.files?.length) {
-            value = isMultiple ? input.files : input.files[0]
+            value = input.multiple ? input.files : input.files[0]
         } else {
             value = undefined;
         }
--- projects/ng0/form/form.module.ts
+++ projects/ng0/form/form.module.ts
@@ -14,7 +14,6 @@ const items = [
 
 /**
  * FormModule
- * @description
  * 
  */
 @NgModule({
